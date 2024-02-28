c548a4ee0b30ce16be3f9cd878373e58e9c458d0
guille-natel <guillermina.natel@mercadolibre.com>
2024-02-28 18:43:23 -0300

    Resolucion de ejercicio2

---
+++ ejercicio2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var temperatura int
+	var humedad int
+	var presionAtmosferica float64
+
+	fmt.Printf("El tipo de temperatura es: %T\n", temperatura)
+	fmt.Printf("El tipo de humedad es: %T\n", humedad)
+	fmt.Printf("El tipo de presion atmosferica es: %T\n", presionAtmosferica)
+
+}
