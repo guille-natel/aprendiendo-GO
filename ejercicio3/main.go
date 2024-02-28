0a4d1bfacefc375224d087ecafb3a1ffd6d57dd3
guille-natel <guillermina.natel@mercadolibre.com>
2024-02-28 18:43:56 -0300

    Resolucion de ejercicio3

---
+++ ejercicio3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// var 1firstName string
+
+	var lastName string
+ 
+	var int age
+ 
+	// 1lastName := 6 // incorrecto 
+ 
+	/var driver_license = true // incorrecto 
+ 
+	// var person height int // incorrecto
+ 
+	// childsNumber := 2
+
+	
+
+}
