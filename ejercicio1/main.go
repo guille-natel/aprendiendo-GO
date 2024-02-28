1f7a3aed2d0121cdcd231458aac7ce44ad619b0b
guille-natel <guillermina.natel@mercadolibre.com>
2024-02-28 18:40:27 -0300

    Resolucion de ejercicio1

---
+++ ejercicio1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre string = "Guillermina"
+	var direccion string = "Mendoza,Argentina"
+
+	fmt.Println(nombre)
+	fmt.Println(direccion)
+
+}
